9ad1a3c609b6269ee84caf5a50443313707886d1
Aaron Rincon <rlaaron0305@gmail.com>
2022-08-19 02:08:02 -0500

    date app

--- practica1/src/AppDate.java
+++ practica1/src/AppDate.java
@@ -1,17 +1,126 @@
 import java.util.Scanner;
 public class AppDate {
     public static void main(String[] args) {
-        Date date = new Date(10, 1, 1900);
+        Date date = new Date(10, 5, 2018);
         Scanner sn = new Scanner(System.in);
-        // System.out.println(date.shortDate());
-        System.out.println(date.daysBetewen());
-        // System.out.println(date.dayWeek());
-        System.out.println(date.longDate());
-        System.out.println("Enter a days to agree: ");
-        int days = sn.nextInt();
-        System.out.println(date.dateAfter(days));
-        sn.close();
+        boolean exit = false;
+        int op; 
+        while(!exit){
+           // Printing the menu.
+            System.out.println("\n1.Leap year check");
+            System.out.println("2.Days in a month");
+            System.out.println("3.Short date");
+            System.out.println("4.Days since 1-1-1900 until date input");
+            System.out.println("5.Day of week");
+            System.out.println("6.Long date");
+            System.out.println("7.Date after days");
+            System.out.println("8.Next day");
+            System.out.println("9.Before day");
+            System.out.println("10.Getters and setters");
+            System.out.println("11.Exit");
+            try {
+                System.out.println("Select an option");
+                op = sn.nextInt();
+                switch (op){
+                    case 1:
+                        System.out.println("Enter check leap year: ");
+                        int year = sn.nextInt();
+                        System.out.println(date.leapYeear(year));
+                        break;
+                    case 2:
+                        System.out.println("Enter month and year to count days: ");
+                        int month = sn.nextInt();
+                        int yearDays = sn.nextInt();
+                        System.out.println(date.days(month, yearDays));
+                        break;
+                    case 3:
+                        System.out.println("Short date: ");
+                        System.out.println(date.shortDate());
+                        break;
+                    case 4:
+                        System.out.println("Days since 1-1-1900 until date input: ");
+                        System.out.println(date.daysBetewen());
+                        break;
+                    case 5:
+                        System.out.println("Day of week: ");
+                        System.out.println(date.dayWeek());
+                        break;
+                    case 6:
+                        System.out.println("Long date: ");
+                        System.out.println(date.longDate());
+                        break;
+                    case 7:
+                        System.out.println("Date after days: ");
+                        int days = sn.nextInt();
+                        System.out.println(date.dateAfter(days));
+                        break;
+                    case 8:
+                        System.out.println("Next day: ");
+                        System.out.println(date.nextDay());
+                        break;
+                    case 9:
+                        System.out.println("Before day: ");
+                        System.out.println(date.backDay());
+                        break;
+                    case 10:
+                        while(!exit){
+                            System.out.println("\n1.Set day");
+                            System.out.println("2.Set month");
+                            System.out.println("3.Set year");
+                            System.out.println("4.Get day");
+                            System.out.println("5.Get month");
+                            System.out.println("6.Get year");
+                            System.out.println("7.Get leap year");
+                            System.out.println("9.Exit");
+                            System.out.println("Select an option");
+                            op = sn.nextInt();
+                            switch (op){
+                                case 1:
+                                    System.out.println("Enter day: ");
+                                    int day = sn.nextInt();
+                                    date.setDay(day);
+                                    break;
+                                case 2:
+                                    System.out.println("Enter month: ");
+                                    int monthS = sn.nextInt();
+                                    date.setMonth(monthS);
+                                    break;
+                                case 3:
+                                    System.out.println("Enter year: ");
+                                    int yearS = sn.nextInt();
+                                    date.setYear(yearS);
+                                    break;
+                                case 4:
+                                    System.out.println("Day: " + date.getDay());
+                                    break;
+                                case 5:
+                                    System.out.println("Month: " + date.getMonth());
+                                    break;
+                                case 6:
+                                    System.out.println("Year: " + date.getYear());
+                                    break;
+                                case 7:
+                                    System.out.println("Leap year: " + date.getLeapYear());
+                                    break;
+                                case 8:
+                                    exit = true;
+                                    break;
+                                default:
+                                    System.out.println("Invalid option");
+                                    break;
+                            }
+                        }
+                        break;
+                    case 11:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid option");
+            }
+        } sn.close();
     }
-
-    
 }
--- practica1/src/Date.java
+++ practica1/src/Date.java
@@ -1,9 +1,11 @@
 public class Date {
+    // Declaring the variables that will be used in the class.
     private int day;
     private int month;
     private int year;
     private boolean leapYear;
 
+    // The default constructor.
     public Date(){
         day = 1;
         month = 1;
@@ -11,6 +13,10 @@ public class Date {
         leapYear = false;
     }
 
+    // A constructor that takes three parameters, day, month and year. It checks if the parameters are
+    // valid and if they are, it sets the values of the variables day, month and year to the
+    // parameters. If the parameters are not valid, it sets the values of the variables day, month and
+    // year to 1, 1 and 1900 respectively.
     public Date(int day, int month, int year){
         if (day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0){
             this.day = day;
@@ -30,6 +36,48 @@ public class Date {
         
     }
 
+    // getters.
+   public int getDay(){
+       return day;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public boolean getLeapYear(){
+        return leapYear;
+    }
+
+    // setters.
+    public void setDay(int day){
+        this.day = day;
+    }
+
+    public void setMonth(int month){
+        this.month = month;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+        if(leapYeear(year)){
+            leapYear = true;
+        }
+    }
+
+
+
+    /**
+     * If the year is divisible by 4, but not by 100, or if the year is divisible by 400, then it is a
+     * leap year
+     * 
+     * @param year the year to check
+     * @return leapYear
+     */
     public boolean leapYeear(int year){
         if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             leapYear = true;
@@ -39,6 +87,13 @@ public class Date {
         return leapYear;
     }
 
+    /**
+     * This function takes in a month and a year and returns the number of days in that month
+     * 
+     * @param month the month you want to know the days of
+     * @param year the year of the date
+     * @return The number of days in a month.
+     */
     public int days(int month, int year){
         int days = 0;
         if(month==1 || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
@@ -52,16 +107,18 @@ public class Date {
                 days = 28;
             }
         }
+        System.out.println("Days passed: ");
         return days;
     }
+    /**
+     * @return The day, month and year of the date.
+     */
     public String shortDate(){
         return day + "/" + month + "/" + year;
     }
-
-    public boolean leapyear(){
-        
-        return leapYear;
-    }
+    /**
+     * @return The number of days between 1-1-1900 and the date.
+     */
 
     public int daysBetewen(){
         int days = -1;
@@ -79,6 +136,13 @@ public class Date {
 
         return days;
     }
+
+    /**
+     * It takes the number of days between the two dates and uses the modulus operator to determine the
+     * day of the week
+     * 
+     * @return The day of the week.
+     */
     public String dayWeek(){
         String dayWeek = "";
         int days = daysBetewen();
@@ -100,6 +164,9 @@ public class Date {
         return dayWeek;
     }
     
+    /**
+     * @return The day, month and year of the date.
+     */
     public String longDate(){
         String longDate = "";
         longDate = dayWeek() + " " + day;
@@ -132,7 +199,14 @@ public class Date {
         longDate += " " + year;
         return longDate;
     }
-    
+
+
+    /**
+     * It takes a date and adds a number of days to it, returning the new date
+     * 
+     * @param days The number of days to add to the current date.
+     * @return The date after the number of days that is passed in.
+     */
     public String dateAfter(int days){
         int newDay = day + days;
         int newMonth = month;
@@ -148,6 +222,13 @@ public class Date {
         return newDay + "/" + newMonth + "/" + newYear;
     }
 
+    /**
+     * If the day is greater than the number of days in the month, subtract the number of days in the
+     * month from the day, increment the month, and if the month is greater than 12, subtract 12 from
+     * the month and increment the year
+     * 
+     * @return The next day.
+     */
     public String nextDay(){
         int newDay = day + 1;
         int newMonth = month;
@@ -163,6 +244,13 @@ public class Date {
         return newDay + "/" + newMonth + "/" + newYear;
     }
 
+    /**
+     * If the day is greater than 1, subtract 1 from the day. If the day is less than 1, subtract 1
+     * from the month and add the number of days in the previous month to the day. If the month is less
+     * than 1, subtract 1 from the year and add 12 to the month
+     * 
+     * @return The day, month, and year of the previous day.
+     */
     public String backDay(){
         int newDay = day - 1;
         int newMonth = month;
