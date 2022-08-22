a2eda51f66a23297c91c1759c465695a61b69d45
Aaron Rincon <rlaaron0305@gmail.com>
2022-08-22 17:33:14 -0500

    class fraction

--- practica dos/src/Fraction.java
+++ practica dos/src/Fraction.java
@@ -28,4 +28,50 @@ public class Fraction {
         this.denominator = denominator;
     }
 
+    public Fraction add(Fraction a, Fraction b){
+        int newNumerator = ((a.getNumerator()*b.getDenominator())+(a.getDenominator()*b.getNumerator()));
+        int newDenominator = ((a.getDenominator()*b.getDenominator()));
+        Fraction add = new Fraction(newNumerator, newDenominator);
+        return add;
+    }
+
+    public Fraction subtract(Fraction a, Fraction b){
+        int newNumerator = ((a.getNumerator()*b.getDenominator())-(a.getDenominator()*b.getNumerator()));
+        int newDenominator = ((a.getDenominator()*b.getDenominator()));
+        Fraction subtract = new Fraction(newNumerator, newDenominator);
+        return subtract;
+    }
+
+    public Fraction multiply(Fraction a, Fraction b){
+        int newNumerator = (a.getNumerator()*b.getNumerator());
+        int newDenominator = (a.getDenominator()*b.getDenominator());
+        Fraction multiply = new Fraction(newNumerator, newDenominator);
+        return multiply;
+    }
+
+    public Fraction divide(Fraction a, Fraction b){
+        int newNumerator = (a.getNumerator()*b.getDenominator());
+        int newDenominator = (a.getDenominator()*b.getNumerator());
+        Fraction divide = new Fraction(newNumerator, newDenominator);
+        return divide;
+    }
+
+    public Fraction simplify(Fraction a){
+        int newNumerator = a.getNumerator();
+        int newDenominator = a.getDenominator();
+        for(int i = newNumerator; i>0; i--){
+            if(newNumerator%i==0 && newDenominator%i==0){
+                newNumerator = newNumerator/i;
+                newDenominator = newDenominator/i;
+            }
+        }
+        Fraction simplify = new Fraction(newNumerator, newDenominator);
+        return simplify;
+    } 
+
+    @Override
+    public String toString(){
+        return numerator+"/"+denominator;
+    }
+
 }
