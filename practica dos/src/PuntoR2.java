8273f07df86f663a8525319910cf20efb792c675
Aaron Rincon <rlaaron0305@gmail.com>
2022-08-22 12:41:44 -0500

    create PuntoR2

---
+++ practica dos/src/PuntoR2.java
@@ -0,0 +1,34 @@
+public class PuntoR2 {
+    private int x;
+    private int y;
+
+    public PuntoR2(){
+        this.x=0;
+        this.y=0;
+    }
+
+    public PuntoR2(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters
+    public int getPx(){
+        return x;
+    }
+
+    public int getPy(){
+        return y;
+    }
+    
+    //setters
+    public void setPx(int x){
+        this.x = x;
+    }
+
+    public void setPy(int y){
+        this.x = y;
+    }
+
+    
+}
