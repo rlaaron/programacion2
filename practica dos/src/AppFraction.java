450dd01867a92193d5cb150c4ee61bb7f7964b3c
Aaron Rincon <rlaaron0305@gmail.com>
2022-08-29 03:20:28 -0500

    add app fraction

---
+++ practica dos/src/AppFraction.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class AppFraction {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        Fraction a = new Fraction(12,4);
+        Fraction b = new Fraction(10,5);
+        Scanner input = new Scanner(System.in);
+        int menuMain; 
+        boolean exit = false;        
+        boolean exit2 = false;        
+        do {
+            menuMain = menu.menuFraction();
+            switch(menuMain){
+                case 1: 
+                    do {
+                        int subMenu = menu.subMenuFraction();
+                        switch(subMenu){
+                            case 1:
+                                System.out.println("Enter numerator:");
+                                a.setNumerator(menu.input.nextInt());
+                                System.out.println("Enter denominator:");
+                                a.setDenominator(menu.input.nextInt());
+                                break;
+                            case 2:
+                                System.out.println("Enter numerator:");
+                                b.setNumerator(menu.input.nextInt());
+                                System.out.println("Enter denominator:");
+                                b.setDenominator(menu.input.nextInt());
+                                break;
+                            case 3:
+                                exit2 = true;
+                                break;
+                            default:
+                                System.out.println("Invalid option");
+                                break;
+                        }
+                    } while (!exit2); 
+                    break;
+                case 2:
+                    System.out.println(a.add(a,b));
+                    break;
+                case 3:
+                    System.out.println(a.subtract(a,b));
+                    break;
+                case 4:
+                    System.out.println(a.multiply(a,b));
+                    break;
+                case 5:
+                    System.out.println(a.divide(a,b));
+                    break;
+                case 6:
+                    System.out.println(a.simplify(a));
+                    break;
+                case 7:
+                    String fractions = "Fraction 1: " + a.toString()+" Fraction 2: " + b.toString();
+                    System.out.println(fractions);
+                    break;
+                case 8:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }while(!exit);
+        input.close();
+
+    }
+}
