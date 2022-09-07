3ce3cb8964c50d3d457d1a71728ccedd7a774121
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-06 19:27:09 -0500

    menutriangulo

--- practica tres/src/MenuTriangulo.java
+++ practica tres/src/MenuTriangulo.java
@@ -3,23 +3,23 @@ public class MenuTriangulo {
     public static void main(String[] args) {
         Scanner input= new Scanner (System.in);
         //Se crean los 3 puntos 
-        Punto p1= new Punto();
-        Punto p2= new Punto();
-        Punto p3= new Punto();
+        Punto p1= new Punto(10,50);
+        Punto p2= new Punto(20,30);
+        Punto p3= new Punto(60,80);
         
       //Se le pide al usuario que ingrese las coordenadas de los 3 puntos 
-        System.out.println ("Ingrese x del punto 1: ");
-        p1.setX(input.nextDouble());
-        System.out.println("Ingrese y del punto 1: ");
-        p1.setY(input.nextDouble());
-        System.out.println("Ingrese x del punto 2: ");
-        p2.setX(input.nextDouble());
-        System.out.println("Ingrese y del punto 2: ");
-        p2.setY(input.nextDouble());
-        System.out.println("Ingrese x del punto 3: ");
-        p3.setX(input.nextDouble());
-        System.out.println("Ingrese y del punto 3: ");
-        p3.setX(input.nextDouble());
+        // System.out.println ("Ingrese x del punto 1: ");
+        // p1.setX(input.nextDouble());
+        // System.out.println("Ingrese y del punto 1: ");
+        // p1.setY(input.nextDouble());
+        // System.out.println("Ingrese x del punto 2: ");
+        // p2.setX(input.nextDouble());
+        // System.out.println("Ingrese y del punto 2: ");
+        // p2.setY(input.nextDouble());
+        // System.out.println("Ingrese x del punto 3: ");
+        // p3.setX(input.nextDouble());
+        // System.out.println("Ingrese y del punto 3: ");
+        // p3.setX(input.nextDouble());
         
         //Se crea el nuevo triangulo con los puntos 
         Triangulo t= new Triangulo(p1,p2,p3);
