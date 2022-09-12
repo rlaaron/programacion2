f110d75f75d8d801a0c82b52c1c4fb610355fc08
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-12 15:56:49 -0500

    constructor class date

--- practica cinco/src/Date.java
+++ practica cinco/src/Date.java
@@ -1,4 +1,17 @@
 public class Date {
+    private int day;
+    private int month;
+    private int year; 
+
+    public Date(int day, int month, int year){
+        this.day = day;
+        this.month=month;
+        this.year=year;
+    }
     
+    @Override
+    public String toString(){
+        return day+"/"+" "+month+"/"+" "+year;
+    }
 }
  
