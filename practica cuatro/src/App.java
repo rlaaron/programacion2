0eea0b7c4e5142c9093a97fa37ddfc27d99e3cce
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-12 15:42:55 -0500

    app class

---
+++ practica cuatro/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Occurrences oc = new Occurrences("Hello World");
+        // System.out.println(oc.changeOcurrence("Hello", "perro"));
+        Rhombus rh = new Rhombus(3, new String[] { "A", "R", "L" });
+        // Rhombus rh = new Rhombus(3, "arl");
+        rh.printRhombus();
+        // System.out.println(rh.print());
+        // Reverse rev = new Reverse(new String[] { "Aaron", "Rincon" });
+        // System.out.println(rev.reverseString());
+        // System.out.println(rev.print());
+    }
+}
