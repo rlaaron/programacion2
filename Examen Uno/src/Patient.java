8540d0a901af28e7f0492c2bacaa4d5a7d98acaf
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-23 01:29:14 -0500

    constructor create

--- Examen Uno/src/Patient.java
+++ Examen Uno/src/Patient.java
@@ -1,3 +1,46 @@
 public class Patient {
-    
+    private int id,age,metricsCount;
+    private String name, gender;
+    private Metric[] metrics;
+
+    public Patient (int id, int age, int metricsCount, String name, String gender){
+        this.id = id;
+        this.age = age;
+        this.metricsCount = metricsCount;
+        this.name = name;
+        this.gender = gender;
+        metrics = new Metric[100];
+        metricsCount = 0;
+    }
+
+    //setters and getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMetricsCount() {
+        return metricsCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
