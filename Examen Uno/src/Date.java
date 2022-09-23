82425760a572530a236041bfcffabe721819746a
Aaron Rincon <rlaaron0305@gmail.com>
2022-09-23 01:29:03 -0500

    date class write full

--- Examen Uno/src/Date.java
+++ Examen Uno/src/Date.java
@@ -1,3 +1,46 @@
 public class Date {
-    
+    private int day, month, year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public Date() {
+        this.day = 0;
+        this.month = 0;
+        this.year = 0;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+
+    public String toString() {
+        return "Date [day=" + day + ", month=" + month + ", year=" + year + "]";
+    }
+
 }
