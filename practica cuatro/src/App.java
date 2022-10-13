8371b2259df3664afecf603d9fc871e37c183552
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-13 01:25:22 -0500

    main with comment functions

--- practica cuatro/src/App.java
+++ practica cuatro/src/App.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        // Occurrences oc = new Occurrences("Hello World");
-        // System.out.println(oc.changeOcurrence("Hello", "perro"));
-        Rhombus rh = new Rhombus(3, new String[] { "A", "R", "L" });
-        // Rhombus rh = new Rhombus(3, "arl");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("ingrese la frase: ");
+        // String frase = sc.nextLine();
+        // Occurrences oc = new Occurrences(frase);
+        // System.out.println("ingrese la palabra a cambiar: ");
+        // String old = sc.nextLine();
+        // System.out.println("ingrese la nueva palabra: ");
+        // String newW = sc.nextLine();
+        // System.out.println(oc.changeOcurrence(old, newW));
+        Rhombus rh = new Rhombus("aborígenesd");
+        // Rhombus rh = new Rhombus("erika");
         rh.printRhombus();
+        // Rhombus rh = new Rhombus(3, "arl");
         // System.out.println(rh.print());
         // Reverse rev = new Reverse(new String[] { "Aaron", "Rincon" });
         // System.out.println(rev.reverseString());
