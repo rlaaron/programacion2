6cd965bbcbafd1379f74e2536da63bbb4edb86c7
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-13 00:08:35 -0500

    class children

---
+++ practica ocho/src/TermometroFahrenheit.java
@@ -0,0 +1,32 @@
+public class TermometroFahrenheit extends Termometro implements Temperatura {
+    public TermometroFahrenheit(double grados) {
+        super(grados);
+    }
+
+    @Override
+    public double convertir(double grados) {
+        return (grados-32)*5/9;
+    }
+
+    @Override
+    public String determinarTemperatura() {
+        if (getGrados() < 0) {
+            return "Congelado";
+        } else if (getGrados() < 10) {
+            return "Frio";
+        } else if (getGrados() < 20) {
+            return "Templado";
+        } else if (getGrados() < 30) {
+            return "Calido";
+        } else {
+            return "Caluroso";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Temperatura actual: " + convertir(getGrados()) + "F";
+    }
+}
+    
+
