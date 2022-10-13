29d4057ba5be9c20dceb4ecc82ac851dcd89274c
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-13 00:08:21 -0500

    classs children

---
+++ practica ocho/src/TermometroCelsius.java
@@ -0,0 +1,32 @@
+public class TermometroCelsius extends Termometro implements Temperatura {
+    public TermometroCelsius(double grados) {
+        super(grados);
+    }
+
+    @Override
+    public double convertir(double grados) {
+        return (grados*9/5)+32;
+    }
+
+    @Override
+    public String determinarTemperatura() {
+        if (getGrados() < 0) {
+            return "Congelado";
+        } else if (getGrados() < 10) {
+            return "Frio";
+        } else if (getGrados() < 20) {
+            return "Templado";
+        } else if (getGrados() < 30) {
+            return "Calido";
+        } else {
+            return "Caluroso";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Temperatura actual: " + convertir(getGrados()) + "C";
+    }
+}
+    
+
