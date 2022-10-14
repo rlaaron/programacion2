1bf4f55f4ed4151d0605723f13aa4cda82cb5332
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-14 16:36:50 -0500

    update

--- Examen Dos/src/Cat.java
+++ Examen Dos/src/Cat.java
@@ -1,8 +1,8 @@
 public class Cat extends Animal {
     private int lives;
 
-    public Cat(String name, int age, double price) {
-        super(name, age, price);
+    public Cat(String name, double price) {
+        super(name, price);
         this.lives = 7;
     }
 
@@ -14,8 +14,30 @@ public class Cat extends Animal {
         this.lives = lives;
     }
 
+    public void kill(){
+        if (this.lives > 0){
+            this.lives--;
+            discount();
+        }else{
+            System.out.println("The cat is already dead");
+        }
+    }
+
+    public void discount(){
+        if (this.lives == 0){
+            this.setPrice(0);
+        }else{
+            this.setPrice(this.getPrice() - (this.getPrice() * 0.10));
+        }
+    }
+
     @Override
     public void sound() {
-        System.out.println("Miau");
+        System.out.print("Miau");
+    }
+
+    @Override
+    public String toString(){
+        return " Im " + getName() + " a CAT and i have " + lives + " lives and my price is " + getPrice();
     }
 }
