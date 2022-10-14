fea88a3fbc5bbfbf64311c22c6688608cb4835ec
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-14 16:39:02 -0500

    change menus, using poliformis

--- practica ocho/src/Termometros.java
+++ practica ocho/src/Termometros.java
@@ -5,6 +5,7 @@ public class Termometros {
         boolean salir = false;
         boolean salir2 = false;
         int opt, opt2;
+        Termometro t = null;
         do {
             System.out.println("1. Termometro Celsius");
             System.out.println("2. Termometro Fahrenheit");
@@ -15,93 +16,14 @@ public class Termometros {
                 case 1:
                     System.out.println("Ingrese la temperatura: ");
                     double grados = sc.nextDouble();
-                    TermometroCelsius tc = new TermometroCelsius(grados);
+                    t = new TermometroCelsius(grados);
                     System.out.println("Termometro creado con exito: ");
-                    do {
-                        System.out.println("1.Convertir a Fahrenheit");
-                        System.out.println("2.Determinar temperatura");
-                        System.out.println("3.Guardar memoria");
-                        System.out.println("4.Mostrar temperatura minima");
-                        System.out.println("5.Mostrar temperatura maxima");
-                        System.out.println("6.Resetear memoria");
-                        System.out.println("7.Salir");
-                        System.out.print("Opcion: ");
-                        opt2 = sc.nextInt();
-                        switch (opt2) {
-                            case 1:
-                                System.out.println("Temperatura en Fahrenheit: " + tc.convertir(tc.getGrados()));
-                                break;
-                            case 2:
-                                System.out.println("Temperatura: " + tc.determinarTemperatura());
-                                break;
-                            case 3:
-                                tc.guardarMemoria();
-                                System.out.println("Memoria guardada con exito");
-                                break;
-                            case 4:
-                                System.out.println("Temperatura minima: " + tc.minimo());
-                                break;
-                            case 5:
-                                System.out.println("Temperatura maxima: " + tc.maximo());
-                                break;
-                            case 6:
-                                tc.resetearMemoria();
-                                System.out.println("Memoria reseteada con exito");
-                                break;
-                            case 7:
-                                salir2 = true;
-                                break;
-                            default:
-                                System.out.println("Opcion invalida");
-                                break;
-                        }
-
-                    } while (salir2);
                     break;
                 case 2:
                     System.out.println("Ingrese la temperatura: ");
                     double grados2 = sc.nextDouble();
-                    TermometroFahrenheit tf = new TermometroFahrenheit(grados2);
+                    t = new TermometroFahrenheit(grados2);
                     System.out.println("Termometro creado con exito: ");
-                    do {
-                        System.out.println("1.Convertir a Celsius");
-                        System.out.println("2.Determinar temperatura");
-                        System.out.println("3.Guardar memoria");
-                        System.out.println("4.Mostrar temperatura minima");
-                        System.out.println("5.Mostrar temperatura maxima");
-                        System.out.println("6.Resetear memoria");
-                        System.out.println("7.Salir");
-                        System.out.print("Opcion: ");
-                        opt2 = sc.nextInt();
-                        switch (opt2) {
-                            case 1:
-                                System.out.println("Temperatura en Celsius: " + tf.convertir(tf.getGrados()));
-                                break;
-                            case 2:
-                                System.out.println("Temperatura: " + tf.determinarTemperatura());
-                                break;
-                            case 3:
-                                tf.guardarMemoria();
-                                System.out.println("Memoria guardada con exito");
-                                break;
-                            case 4:
-                                System.out.println("Temperatura minima: " + tf.minimo());
-                                break;
-                            case 5:
-                                System.out.println("Temperatura maxima: " + tf.maximo());
-                                break;
-                            case 6:
-                                tf.resetearMemoria();
-                                System.out.println("Memoria reseteada con exito");
-                                break;
-                            case 7:
-                                salir2 = true;
-                                break;
-                            default:
-                                System.out.println("Opcion invalida");
-                                break;
-                        }
-                    } while (salir2);
                     break;
                 case 3:
                     salir = true;
@@ -110,6 +32,45 @@ public class Termometros {
                     System.out.println("Opcion invalida");
                     break;
             }
+            do {
+                // System.out.println("1.Convertir" + instanceof TermometroCelsius ? System.out.println("Farenhet") :  System.out.println("Celsius") + t.convertir(t.getGrados()));
+                System.out.println("2.Determinar temperatura");
+                System.out.println("3.Guardar memoria");
+                System.out.println("4.Mostrar temperatura minima");
+                System.out.println("5.Mostrar temperatura maxima");
+                System.out.println("6.Resetear memoria");
+                System.out.println("7.Salir");
+                System.out.print("Opcion: ");
+                opt2 = sc.nextInt();
+                switch (opt2) {
+                    case 1:
+                        // System.out.println("Temperatura en " + instanceof TermometroCelsius ? System.out.println("Farenhet") :  System.out.println("Celsius") + t.convertir(t.getGrados()));
+                        break;
+                    case 2:
+                        System.out.println("Temperatura: " + t.determinarTemperatura());
+                        break;
+                    case 3:
+                        t.guardarMemoria();
+                        System.out.println("Memoria guardada con exito");
+                        break;
+                    case 4:
+                        System.out.println("Temperatura minima: " + t.minimo());
+                        break;
+                    case 5:
+                        System.out.println("Temperatura maxima: " + t.maximo());
+                        break;
+                    case 6:
+                        t.resetearMemoria();
+                        System.out.println("Memoria reseteada con exito");
+                        break;
+                    case 7:
+                        salir2 = true;
+                        break;
+                    default:
+                        System.out.println("Opcion invalida");
+                        break;
+                }
+            } while (salir2);
         } while (!salir); sc.close();
     }
 }
