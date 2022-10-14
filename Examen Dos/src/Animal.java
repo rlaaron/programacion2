40ed119682747dceb24b9884a969711ebb2b7b9e
Aaron Rincon <rlaaron0305@gmail.com>
2022-10-14 16:36:33 -0500

    update abstract

--- Examen Dos/src/Animal.java
+++ Examen Dos/src/Animal.java
@@ -1,22 +1,15 @@
-public abstract class Animal implements Sound {
+public class Animal implements Sound {
     private String name;
-    private int age;
     private double price;
 
-    public Animal(String name, int age, double price) {
+    public Animal(String name, double price) {
         this.name = name;
-        this.age = age;
         this.price = price;
     }
 
     public String getName() {
         return name;
     }
-
-    public int getAge() {
-        return age;
-    }
-
     public double getPrice() {
         return price;
     }
@@ -25,26 +18,17 @@ public abstract class Animal implements Sound {
         this.name = name;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public void setPrice(double price) {
         this.price = price;
     }
 
-    // public void sound() {
-    //     System.out.println("Sound");
-    // }
-
-    public abstract void sound();
+    // public abstract void sound();
 
     @Override
     public String toString() {
         return "Animal{" +
                 "name='" + name + '\'' +
-                ", age=" + age +
-                ", price=" + price +
+                " price=" + price +
                 '}';
     }
 
